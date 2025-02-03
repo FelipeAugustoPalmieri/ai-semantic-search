17170b0b6c5ad8e08361e37d38f170cab09f1bc1
FelipeAugustoPalmieri <felipepalmieridev@gmail.com>
2025-02-03 17:53:02 -0300

    Has been added function load

--- src/loader.ts
+++ src/loader.ts
@@ -5,3 +5,11 @@ import { JSONLoader } from "langchain/document_loaders/fs/json";
 const loader = new DirectoryLoader(path.resolve(__dirname, "../tmp"), {
   ".json": (path) => new JSONLoader(path, "/text"),
 });
+
+async function load() {
+  const docs = await loader.load();
+
+  console.log(docs);
+}
+
+load();
